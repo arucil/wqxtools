5246ceb0bff5044f535254af1ad0678dbb75be2f
amlo <xplzjwz@gmail.com>
2021-12-22 17:55:03 +0800

    feat: about

--- gui/src/about_dialog.cpp
+++ gui/src/about_dialog.cpp
@@ -8,7 +8,13 @@ AboutDialog::AboutDialog(QWidget *parent) : QDialog(parent) {
   auto layout = new QVBoxLayout(this);
   auto about = new QLabel(
     "<p>作者：arucil</p>"
-    "<p>Github仓库：<a>https://github.com/arucil/wqxtools</a></p>");
+    "<p>Github仓库：<a>https://github.com/arucil/wqxtools</a></p>"
+    "<p>开源协议：MIT License</p>"
+    "<br/>"
+    R"(<p>GVBASIC 编辑器的图标来源：<br>
+    Noto Emoji: <a>https://github.com/googlefonts/noto-emoji</a><br>
+    Elementary OS Icons: <a>https://github.com/elementary/icons</a>
+    </p>)");
   about->setTextInteractionFlags(Qt::TextSelectableByMouse);
   about->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
   about->setContentsMargins(15, 15, 15, 15);
